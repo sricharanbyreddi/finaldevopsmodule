3003438dff10eb8aba282192128174a3ff76ed9d
sricharanbyreddi <110220105+sricharanbyreddi@users.noreply.github.com>
2023-10-11 15:01:39 +0530

    Add files via upload

---
+++ src/test/java/com/lbrce/final_module/UserVerificationTest.java
@@ -0,0 +1,23 @@
+package com.lbrce.final_module;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class UserVerificationTest {
+	UserVerification uv=new UserVerification();
+	@Test
+	public void testcase1() {
+		Assert.assertEquals(true,uv.check("usha", "usha@2020"));
+	}
+	@Test
+	public void testcase2() {
+		Assert.assertEquals(true,uv.check("usha", "usha@2020"));
+	}
+	@Test
+	public void testcase3() {
+		Assert.assertEquals(false,uv.check("usha1", "usha@2020"));
+	}
+	@Test
+	public void testcase4() {
+		Assert.assertEquals(false,uv.check("usha1123", "usha@2020"));
+	}
+}
